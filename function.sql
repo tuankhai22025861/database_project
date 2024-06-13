d82fdd5a9c47e56bf3548a6bebf4a6fe99838691
lamtad <145664199+lamtad@users.noreply.github.com>
2024-06-13 18:51:51 +0700

    Update function.sql

--- function.sql
+++ function.sql
@@ -19,3 +19,21 @@ end;
 $$ language plpgsql;
 select * from insert_new_customer('123 Ngo','Quang','1999-9-9','M','quincy')
 select * from customer
+--2.FIND PRODUCT 
+create or replace function find_product(
+	p_category_id int,
+	p_product_name text
+)returns table(
+	product_id int,
+	product_name text,
+	description text,
+	price money,
+	category_id int
+)as $$
+begin 
+	return query
+	select * from product p
+	where p.product_name = p_product_name and p.category_id = p_category_id;
+end;
+$$ language plpgsql;
+
